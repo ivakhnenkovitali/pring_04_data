16b12b6ece7f86307cbe7258cc9cf4aadcd0d4ae
Asus <vaxa2320@gmail.com>
2024-07-24 13:13:37 +0300

    user10

--- src/main/java/by/IT/_02_jpa/configuration/AppConfig.java
+++ src/main/java/by/IT/_02_jpa/configuration/AppConfig.java
@@ -13,7 +13,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import java.util.List;
-
 @EnableJpaRepositories(basePackages = "by.IT._02_jpa.reposotories")
 public class AppConfig {
     @Bean
