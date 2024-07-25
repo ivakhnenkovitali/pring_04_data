360fc07f61bab66192c566a86bf3e8b817a71bc8
Asus <vaxa2320@gmail.com>
2024-07-25 13:15:30 +0300

    user1077

--- src/main/java/by/IT/_02_jpa/Main1.java
+++ src/main/java/by/IT/_02_jpa/Main1.java
@@ -1,19 +1,16 @@
 package by.IT._02_jpa;
-
 import by.IT._02_jpa.configuration.AppConfig;
 import by.IT._02_jpa.entties.Airplane;
 import by.IT._02_jpa.entties.Passenger;
 import by.IT._02_jpa.reposotories.AirplaneRepository;
 import by.IT._02_jpa.reposotories.PassengerRepository;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import java.util.List;
 public class Main1 {
     public static void main(String[] args) {
         var ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         var passRepository = ctx.getBean(PassengerRepository.class);
         var planeRepository = ctx.getBean(AirplaneRepository.class);
-
         var passengers = (List<Passenger>)ctx.getBean("passengers");
         passRepository.saveAll(passengers);
 
